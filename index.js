75f99d2e3dd3f8e8f2fa4cf56f3fc4ae476b9848
Santosh Nepal <santosh.nepal@infodevelopers.com.np>
2022-11-23 17:52:03 +0545

    second commit

--- index.js
+++ index.js
@@ -27,33 +27,6 @@ passport.use(
     });
   })
 );
-// passport.use(
-//   new JWtStrategy(
-//     {
-//       jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
-//       secretOrKey: "my_secret",
-//     },
-//     function (jwtPayload, callback) {
-//       console.log(jwtPayload);
-//       const found = data.filter((_data) => {
-//         if (
-//           _data.username === jwtPayload.username &&
-//           _data.password === jwtPayloadpassword
-//         ) {
-//           return _data;
-//         }
-//       });
-//       if (found.length) {
-//         return callback(null, jwtPayload, {
-//           message: "success in authenticating",
-//         });
-//       }
-//       return callback(null, false, {
-//         message: "error in jwt",
-//       });
-//     }
-//   )
-// );
 app.use(express.json());
 app.post("/login", (req, res, next) => {
   passport.authenticate("local", { session: false }, (err, user, info) => {
@@ -67,16 +40,6 @@ app.post("/login", (req, res, next) => {
     return res.status(200).send({ token: token });
   })(req, res);
 });
-// app.post("/check", (req, res, next) => {
-//   passport.authenticate("jwt", { session: false }, (err, user, info) => {
-//     if (err || !user) {
-//       return res
-//         .status(400)
-//         .json({ message: "sorry couldn't authenticate you" });
-//     }
-//     req.user = user;
-//   })(req, res);
-// });
 app.listen(3000, () => {
   console.log(`server listening on port 3000`);
 });
